fde6041631c8eb4aee9dadd35150d670eeecb9ad
kyoungjooo <leekjoo1008@gmail.com>
2025-03-12 21:18:50 +0900

    chore: 프로젝트 초기 설정

---
+++ README.md
@@ -0,0 +1,11 @@
+## Unsplash Image Api를 활용한 이미지 검색 사이트 만들기
+
+### 개발 환경
+
+- 프로젝트 환경 설정(vite를 활용한 React 프로젝트)설치: `npm create vite@latest`<br>
+- React 중앙집중식 상태관리 라이브러리 Recoil 설치: `npm i recoil`<br>
+- 외부 오픈 API 통신을 위한 라이브러리 설치: `npm i axios`<br>
+- CSS 스타일링을 위한 sass 설치: `npm install sass`<br>
+- React Router 설치: `npm i react-router-dom localforage match-sorter sort-by`<br>
+- React Toast Popup 모듈 설치: `npm i react-simple-toasts`<br>
+- resolve.alias 경로 설정을 위한 설치: `npm i path`
---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@recoil": path.resolve(__dirname, "src/recoil"),
+      "@apis": path.resolve(__dirname, "src/apis"),
+    },
+  },
+  //scss
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@assets/styles/main.scss";`,
+      },
+    },
+  },
+});
